a08281aaafbe2431557b905ff220fb4717fd3e99
Mats Bolstad <mbolstad@gmail.com>
2020-08-31 14:10:16 +0200

    Promise i stedet for async

--- src/proxy/reverse-proxy.js
+++ src/proxy/reverse-proxy.js
@@ -4,9 +4,16 @@ import proxy from 'express-http-proxy';
 const setup = (router, authClient) => {
     router.use("/api", proxy({
         parseReqBody: false,
-        proxyReqOptDecorator: async (options, req) => {
-            const accessToken = await authUtils.getOnBehalfOfAccessToken(authClient, req)
-            options.headers.Authorization = `Bearer ${accessToken}`;
+        proxyReqOptDecorator: (options, req) => {
+            return new Promise(((resolve, reject) =>
+                    authUtils.getOnBehalfOfAccessToken(
+                        authClient, req
+                    ).then(access_token => {
+                            options.headers.Authorization = `Bearer ${access_token}`;
+                            resolve(options)
+                        },
+                        error => reject(error))
+            ))
         },
         proxyReqPathResolver: req => {
             return req.url.replace("/tiltak-refusjon/api", "/tiltak-refusjon-api");

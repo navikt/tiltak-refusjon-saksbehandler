9f9d58b6c30c6b86fbef7cac169e3885cecab9be
Mats Bolstad <mbolstad@gmail.com>
2020-11-09 13:04:45 +0100

    Sesjonshåndtering med Redis

--- server/src/config.js
+++ server/src/config.js
@@ -1,27 +1,27 @@
 import 'dotenv/config';
 
-const envVar = ({name, required = true}) => {
+const envVar = ({ name, required = true }) => {
     if (!process.env[name] && required) {
         console.error(`Missing required environment variable '${name}'`);
         process.exit(1);
     }
-    return process.env[name]
+    return process.env[name];
 };
 
 const server = {
-    host: envVar({name: "HOST", required: false}) || 'localhost', // should be equivalent to the URL this application is hosted on for correct CORS origin header
-    port: envVar({name: "PORT", required: false}) || 3000,
-    proxy: envVar({name: "HTTP_PROXY", required: false}), // optional, only set if requests to Azure AD must be performed through a corporate proxy (i.e. traffic to login.microsoftonline.com is blocked by the firewall)
-    sessionKey: envVar({name: "SESSION_KEY"}), // should be set to a random key of significant length for signing session ID cookies
-    cookieName: 'security-blueprints-login'
+    host: envVar({ name: 'HOST', required: false }) || 'localhost', // should be equivalent to the URL this application is hosted on for correct CORS origin header
+    port: envVar({ name: 'PORT', required: false }) || 3000,
+    proxy: envVar({ name: 'HTTP_PROXY', required: false }), // optional, only set if requests to Azure AD must be performed through a corporate proxy (i.e. traffic to login.microsoftonline.com is blocked by the firewall)
+    sessionKey: envVar({ name: 'SESSION_KEY' }), // should be set to a random key of significant length for signing session ID cookies
+    cookieName: 'tiltak-refusjon-session-id',
 };
 
 const azureAd = {
-    discoveryUrl: envVar({name: "AZURE_APP_WELL_KNOWN_URL"}),
-    clientId: envVar({name: "AZURE_APP_CLIENT_ID"}),
-    clientJwks: JSON.parse(envVar({name: "AZURE_APP_JWKS"})),
-    redirectUri: envVar({name: "AAD_REDIRECT_URL"}),
-    logoutRedirectUri: envVar({name: "AAD_LOGOUT_REDIRECT_URL"}),
+    discoveryUrl: envVar({ name: 'AZURE_APP_WELL_KNOWN_URL' }),
+    clientId: envVar({ name: 'AZURE_APP_CLIENT_ID' }),
+    clientJwks: JSON.parse(envVar({ name: 'AZURE_APP_JWKS' })),
+    redirectUri: envVar({ name: 'AAD_REDIRECT_URL' }),
+    logoutRedirectUri: envVar({ name: 'AAD_LOGOUT_REDIRECT_URL' }),
     tokenEndpointAuthMethod: 'private_key_jwt',
     responseTypes: ['code'],
     responseMode: 'query',
@@ -29,18 +29,18 @@ const azureAd = {
 };
 
 const redis = {
-    host: envVar({name: "REDIS_HOST", required: false}),
-    port: envVar({name: "REDIS_PORT", required: false}) || 6379,
-    password: envVar({name: "REDIS_PASSWORD", required: false})
+    host: envVar({ name: 'REDIS_HOST', required: false }),
+    port: envVar({ name: 'REDIS_PORT', required: false }) || 6379,
+    password: envVar({ name: 'REDIS_PASSWORD', required: false }),
 };
 
 const apiConfig = () => {
     console.log(`Loading reverse proxy config from API_* [CLIENT_ID, URL]`);
-    const scopes = envVar({name: "API_SCOPES", required: false});
+    const scopes = envVar({ name: 'API_SCOPES', required: false });
     return {
-        clientId: envVar({name: "API_CLIENT_ID"}),
-        url: envVar({name: "API_URL"}),
-        scopes: scopes ? scopes.split(',') : []
+        clientId: envVar({ name: 'API_CLIENT_ID' }),
+        url: envVar({ name: 'API_URL' }),
+        scopes: scopes ? scopes.split(',') : [],
     };
 };
 
@@ -48,5 +48,5 @@ export default {
     server,
     azureAd,
     api: apiConfig(),
-    redis
+    redis,
 };

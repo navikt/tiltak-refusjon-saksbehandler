c2557e7fd1d80bc3beace18aa91b897bacbaed82
Mats Bolstad <mbolstad@gmail.com>
2021-06-25 10:25:11 +0200

    Logger til konsoll, slik som utside-appen
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,13 +1,5 @@
-import path from 'path';
 import winston from 'winston';
 
-const logger = winston.createLogger({
-    transports: [
-        new winston.transports.File({
-            format: winston.format.json(), //format.combine(format.timestamp(), loggerFormat),
-            filename: path.join(__dirname, '../', 'logs', 'combined.log'),
-        }),
-    ],
+export default winston.createLogger({
+    transports: [new winston.transports.Console()],
 });
-
-export default logger;

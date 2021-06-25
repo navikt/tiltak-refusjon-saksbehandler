e183667185224f282935be501324e8a4627f2669
Mats Bolstad <mbolstad@gmail.com>
2021-06-25 13:29:49 +0200

    Importfiks

--- src/RefusjonSide/RefusjonSide.tsx
+++ src/RefusjonSide/RefusjonSide.tsx
@@ -1,7 +1,4 @@
-import AlertStripe from 'nav-frontend-alertstriper';
-import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import React, { FunctionComponent } from 'react';
-import HvitBoks from '../Komponenter/HvitBoks';
 import { useHentRefusjon } from '../rest-service';
 import { useParams } from 'react-router-dom';
 import './RefusjonSide.less';

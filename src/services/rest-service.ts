95c59b6e16b732afbc8ed0d61252b7891575faae
Mats Bolstad <mbolstad@gmail.com>
2021-07-06 14:09:16 +0200

    Øker timeout til 30sek

--- src/services/rest-service.ts
+++ src/services/rest-service.ts
@@ -11,7 +11,7 @@ export class FeilkodeError extends Error {}
 
 const api = axios.create({
     baseURL: '/api/saksbehandler',
-    timeout: 15000,
+    timeout: 30000,
     withCredentials: true,
     headers: { Pragma: 'no-cache', 'Cache-Control': 'no-cache' },
     validateStatus: (status) => status < 400,

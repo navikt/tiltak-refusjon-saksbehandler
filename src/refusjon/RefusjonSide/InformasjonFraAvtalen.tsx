ea7e6277ff50831c30f8002f8dd9f67423a3df9d
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-09-10 21:19:28 +0200

    fjernet unused vars

--- src/refusjon/RefusjonSide/InformasjonFraAvtalen.tsx
+++ src/refusjon/RefusjonSide/InformasjonFraAvtalen.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Calender, File, FileContent, Money, People, Warning } from '@navikt/ds-icons';
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';

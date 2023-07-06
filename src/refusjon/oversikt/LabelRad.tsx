4a87bb8bfbcd4d532d4ab2b826cba855a2e41052
MagnusRom <65652609+MagnusRom@users.noreply.github.com>
2023-07-06 12:45:11 +0200

    Tabell oppgraderinger (#45)
    * Legg til løpenummer i oversiktstabell
    
    * tabell fix wip
    
    * Fikset litt css på oversikt tabell og rad
    
    * endret arialabel navn
    
    ---------
    
    Co-authored-by: Odd Andreas Sørsæther <oddsor@users.noreply.github.com>

--- src/refusjon/oversikt/LabelRad.tsx
+++ src/refusjon/oversikt/LabelRad.tsx
@@ -7,6 +7,9 @@ const LabelRad = () => {
     const cls = BEMHelper('label-rad');
     return (
         <div className={cls.className} aria-label="rad overkrifter for kolonnene i refusonslisten">
+            <div className={cls.element('kolonne')} id={cls.element('refusjon')}>
+                <Label>Refusjon</Label>
+            </div>
             <div className={cls.element('kolonne')} id={cls.element('veileder')}>
                 <Label>Veileder</Label>
             </div>

b9103eb8f0cd92266747dc69f63fff8ffcec3490
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-09-26 14:54:11 +0200

    Legg til typescript-definisjon for importmeta

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_ENV: 'dev' | 'prod';
+    // more env variables...
+}

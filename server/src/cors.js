f0be1873bf0dd6396c562fe07aa0197a0cd90c26
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-09-30 14:04:17 +0200

    corsbom

--- server/src/cors.js
+++ server/src/cors.js
@@ -1,10 +1,7 @@
 const config = require('./config');
 
 module.exports = () => (req, res, next) => {
-    res.setHeader(
-        'Access-Control-Allow-Origin',
-        config.server().host + ', ' + config.envVar({ name: 'DECORATOR_CORS', required: true })
-    );
+    res.setHeader('Access-Control-Allow-Origin', config.server().host);
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, X-Requested-With');
     return next();
